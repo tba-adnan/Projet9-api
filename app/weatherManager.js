790899ce5a981c9eb0cfaef44033eb01749eb4e1
tebbaa-adnane <tebbaa-adnane>
2022-01-19 23:47:39 +0100

    add && fix

--- app/weatherManager.js
+++ app/weatherManager.js
@@ -18,31 +18,6 @@ class WeatherManager {
         return this.#_currentForecast
     }
 
-    // async fetchForecast(url) {
-    //     var weather = new Weather()
-
-    //     fetch(url)
-    //     .then(response => {
-    //         // handle the response
-    //         return response.json()
-    //     }).then(json => {
-    //         weather.city = json.name
-    //         weather.description = json.weather[0].main
-    //         weather.iconCode = json.weather[0].ic
-    //         weather.temp = json.main.temp
-    //         weather.humidity = json.main.humidity
-    //         this.#_currentForecast = weather
-    //         // console.log(this.#_currentForecast)
-    //     })
-    //     .catch(error => {
-    //         // handle the error
-    //         console.log(error)
-    //     });
-    //     return await weather
-
-    // }
-
-    
 
     async fetchForecast(url) {
 
